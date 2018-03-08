397a5e5b7c3f9bc387739a63093cead2d35a4c0f
Unknown <javier.duarte12@yahoo.com>
2018-03-08 16:04:43 -0600

    work still on progress on aboutus

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,23 +6,45 @@
 
 @section('content')
 
+<style>
+
+p, h1 {
+    color: white;
+
+}
+</style>
+
+
+
+
+
+
+
 <!-- Header Jumbotron -->
 <section id="top" class="jumbotron jumbotron-fluid @if (empty($response)) fullscreen @endif">
-  <div class="container">
-      <div class="row">
-          <img src=""
-      </div>
-          <div class="col">
 
-          </div>
-    </div>
-    <div class="form-group">
-<label for="exampleFormControlTextarea1">Example textarea</label>
-<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-</form>
+
+        <div class="container">
+            <div class="row"">
+                <div class="col-6">
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    <img src= "/images/NewSupermanPic.jpg" class="rounded-circle" width="450px" height="350px">
+                        <h1 class="text-center">Javier Duarte</h1>
+                </div>
+
+                <div class="col-6">
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    <img src="/images/VictorPic.png" class="rounded-circle" width="450px" height="350px">
+                    <h1 class="text-center">Victor Rodriguez</h1>
+                </div>
+                <p>
+                    We are Valley Tech Academy alumni's, eager and overly excited to work as a team and create challenging projects.
+
+                </p>
+            </div>
+        </div>
+
 </section>
-
 
 
 
