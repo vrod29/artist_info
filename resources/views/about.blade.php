f97983de5b3c045cb901e0d8168948ee06faf930
Unknown <javier.duarte12@yahoo.com>
2018-03-08 14:55:10 -0600

    working progress

---
+++ resources/views/about.blade.php
@@ -0,0 +1,32 @@
+@extends('master')
+
+@section('title')
+    1 Stop Music
+@endsection
+
+@section('content')
+
+<!-- Header Jumbotron -->
+<section id="top" class="jumbotron jumbotron-fluid @if (empty($response)) fullscreen @endif">
+  <div class="container">
+      <div class="row">
+          <img src=""
+      </div>
+          <div class="col">
+
+          </div>
+    </div>
+    <div class="form-group">
+<label for="exampleFormControlTextarea1">Example textarea</label>
+<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+</form>
+</section>
+
+
+
+
+
+
+
+@endsection
