37d82e50cb8533d394033edaa8c1c683d3d913f0
Unknown <javier.duarte12@yahoo.com>
2018-03-09 10:49:41 -0600

    updated about us page
    updated about us page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,49 +6,34 @@
 
 @section('content')
 
-<style>
-
-p, h1 {
-    color: white;
-
-}
-</style>
-
-
-
-
-
-
-
-<!-- Header Jumbotron -->
-<section id="top" class="jumbotron jumbotron-fluid @if (empty($response)) fullscreen @endif">
-
-
-        <div class="container">
-            <div class="row"">
-                <div class="col-6">
-                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                    <img src= "/images/NewSupermanPic.jpg" class="rounded-circle" width="450px" height="350px">
-                        <h1 class="text-center">Javier Duarte</h1>
+    <div class="container mt-5 text-center">
+        <div class="row">
+            <div class="col-6 text-center">
+                <div class="card">
+                    <div class="card-body">
+                        <img class="rounded-circle" width="300" height="300" src="/images/NewSupermanPic.jpg" alt="">
+                        <h5 class="card-title">Javier Duarte</h5>
+                    </div>
                 </div>
-
-                <div class="col-6">
-                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                    <img src="/images/VictorPic.png" class="rounded-circle" width="450px" height="350px">
-                    <h1 class="text-center">Victor Rodriguez</h1>
-                </div>
-                <p>
-                    We are Valley Tech Academy alumni's, eager and overly excited to work as a team and create challenging projects.
-
-                </p>
             </div>
+            <div class="col-6 text-center">
+                <div class="card">
+                    <div class="card-body">
+                        <img class="rounded-circle" width="300" height="300" src="/images/VictorPic.png" alt="">
+                        <h5 class="card-title">Victor Rodriguez</h5>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
-
-</section>
-
-
-
-
-
+        <p>
+            We are Valley Tech Academy alumni's that are excited to create and complete tough challenges that push our learning experience to the limit.
+        </br>We come all the way from asking ourselves "What the heck is this?" to "Whoa! I can do this!" to "I actually did it, yea I created it!"
+    </br>It's really about putting all the pieces together and understanding what each code means and does and where it needs to go.
+</br>If we can do this.....then anybody can Code too...no matter what level of education one has.
+</br>Valley Tech Academy...is the place to be!
+        </p>
+    </div>
 
 @endsection
